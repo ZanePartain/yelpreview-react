ae2323635e2644438f9e635f046d1a5816929027
David Brownell <davidabrownell@gmail.com>
2020-04-22 18:35:03 -0700

    Move Provider to App.js. Add react-redux, react-router-dom, and redux dependencies.

--- milestone2/src/App.js
+++ milestone2/src/App.js
@@ -1,37 +1,40 @@
-import React, { Component }from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Yelp  from './components/bizpage/Yelp';
+import Yelp from './components/bizpage/Yelp';
 import UserPage from './components/user_page/UserPage';
 import MyNav from './components/Navbar';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
+import store from "./redux/store";
+import {Provider} from "react-redux";
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
 
-  render(){
-    return (
-      <Router>
-        <MyNav />
-        <div className="App-header">
-            <Switch>
-              <Route exact path="/" component={Yelp}/>
-              <Route exact path="/user" component={UserPage}/>
-            </Switch>
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <MyNav/>
+                    <div className="App-header">
+                        <Switch>
+                            <Route exact path="/" component={Yelp}/>
+                            <Route exact path="/user" component={UserPage}/>
+                        </Switch>
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
