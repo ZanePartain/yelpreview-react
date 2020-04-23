e040a458c9837b0871b248bc3c98030ec943c92d
David Brownell <davidabrownell@gmail.com>
2020-04-22 21:17:14 -0700

    Add redux-thunk middleware

--- milestone2/src/redux/store.js
+++ milestone2/src/redux/store.js
@@ -1,6 +1,7 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import bizReducer from './reducers/biz.reducer';
 import userReducer from "./reducers/user.reducer";
+import thunk from 'redux-thunk';
 
 // Creating the redux store for managing application state
 // export const history = createBrowserHistory();
@@ -15,6 +16,7 @@ const allReducers = combineReducers(
 const store = createStore(
     allReducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
 );
 
 export default store;  
