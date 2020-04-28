3e4273b839ab8a02cd07f3f69d95a889e212b5f6
ZanePartain <zane.partain@wsu.edu>
2020-04-27 20:35:50 -0700

    some minor changes

--- milestone2/src/components/bizpage/BizRow.js
+++ milestone2/src/components/bizpage/BizRow.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import ModalExample from './BizDetailModal';
 
+function distance(lat1,lon1,lat2,lon2) {
+	var R = 6371; // km (change this constant to get miles)
+	var dLat = (lat2-lat1) * Math.PI / 180;
+	var dLon = (lon2-lon1) * Math.PI / 180;
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+		Math.sin(dLon/2) * Math.sin(dLon/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	var d = R * c;
+	if (d>1) return Math.round(d)+"km";
+	else if (d<=1) return Math.round(d*1000)+"m";
+	return d;
+}
+
 class BizRow extends Component{
     constructor(props){
         super(props);
@@ -39,6 +53,8 @@ class BizRow extends Component{
     }
 
     render(){
+
+        //console.log(distance(this.props.biz["latitude"], this.props.biz["longitude"],1,2 ))
         return(
             <tr className="data-row" onClick={this.handleBizClickFetch.bind(this)}>
                <td  title="name" className="td-data">
@@ -57,13 +73,13 @@ class BizRow extends Component{
                     {this.props.val["distance"]}
                 </td>
                 <td title="rating" className="td-data"  style={{paddingRight: '50px'}}>
-                    {this.props.val["rating"]}
+                    {this.props.val["stars"]}
                 </td>
                 <td title="tips" className="td-data"  style={{paddingRight: '50px'}}>
-                    {this.props.val["tips"]}
+                    {this.props.val["num_tips"]}
                 </td>
                 <td title="checkins" className="td-data"  style={{paddingRight: '50px'}}>
-                    {this.props.val["checkins"]}
+                    {this.props.val["num_checkins"]}
                 </td>
 
                 {/** Modal view for business detail */}
